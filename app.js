73ea3a18ca2523fe8f9f44ab31f32195dff57d66
otsomikkila <otso.mikkila@hotmail.fi>
2024-12-27 14:37:38 +0200

    Helper Functions and Unit Tests step 1

---
+++ app.js
@@ -0,0 +1,35 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+
+const middleWare = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    //console.log(result)
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleWare.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleWare.unknownEndpoint)
+app.use(middleWare.errorHandler)
+
+module.exports = app
