527964e912bfe0aac4c1d41f94f73fd36127fe4d
otsomikkila <otso.mikkila@hotmail.fi>
2024-12-27 14:51:38 +0200

    Helper Functions and Unit Tests step 2

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -8,3 +8,52 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithThreeBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '67644c8064adfef6daac6d9c',
+      title: 'mitähän',
+      author: 'Otso',
+      url: 'moi/moi',
+      likes: 4,
+      __v: 0
+    },
+    {
+      _id: '676e9e3d7f803216e044c737',
+      title: 'blogi 2',
+      author: 'Otso',
+      url: 'moi/2',
+      likes: 4,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+  test('when list has three blogs, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithThreeBlogs)
+    assert.strictEqual(result, 13)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  console.log(blogs)
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+  console.log(blogs.map(n => n.likes))
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
