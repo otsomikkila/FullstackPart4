d2193a62cf1fd98979773875abc83aa374c8a6f3
otsomikkila <otso.mikkila@hotmail.fi>
2024-12-27 15:20:23 +0200

    Helper Functions and Unit Tests step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,18 +1,33 @@
 const dummy = (blogs) => {
-  console.log(blogs)
+  //console.log(blogs)
   return 1
 }
 
 const totalLikes = (blogs) => {
-  console.log(blogs)
+  //console.log(blogs)
   const reducer = (sum, item) => {
     return sum + item.likes
   }
-  console.log(blogs.map(n => n.likes))
+  //console.log(blogs.map(n => n.likes))
   return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  let max = 0
+  let result
+  blogs.forEach(n => {
+    if (n.likes > max) {
+      max = n.likes
+      result = n
+    }
+  })
+  //console.log('result: ', result)
+  //console.log('result: ' , { 'title': result.title, 'author': result.author, 'likes': result.likes } )
+  return { 'title': result.title, 'author': result.author, 'likes': result.likes }
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
