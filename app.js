bd0cc7579c5ae4f8188cb129f6dfda57b9d35db6
otsomikkila <otso.mikkila@hotmail.fi>
2025-01-04 19:41:01 +0200

    Blog List Expansion, step 3

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const middleWare = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -28,6 +29,7 @@ app.use(express.json())
 app.use(middleWare.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleWare.unknownEndpoint)
 app.use(middleWare.errorHandler)
