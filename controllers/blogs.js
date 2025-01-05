cbf05eb0e65bb59a160d62868d060e6b47ef57c5
otsomikkila <otso.mikkila@hotmail.fi>
2025-01-05 10:47:38 +0200

    Blog List Expansion, step 7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   //console.log('get request to /api/blogs')
@@ -9,9 +18,11 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  //lisää jokaiseen uuteen blogiin käyttäjä defaultilla eka mikä löytyy
-  const users = await User.find({})
-  const user = users[0]
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     ...request.body,
